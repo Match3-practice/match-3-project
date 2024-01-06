92c6915d282b4a07a8e4cd63f3047c4920ce391b
RobertEdHouse <robert.edwin.house101@gmail.com>
2024-01-06 02:14:44 +0200

    start prototyping grid and crystal logic

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Movement : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    private Vector2 _startTouchPosition = Vector2.zero;
+    private Vector2 _finishTouchPosition = Vector2.zero;
+    public float swipeAngle = 0;
+    public event Action<Direction> SwapAction;
+     public void OnPointerDown(PointerEventData eventData)
+    {
+        _startTouchPosition = eventData.position;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        _finishTouchPosition = eventData.position;
+        CalculateAngle();
+        SwapAction?.Invoke(CalculateDirection());
+    }
+
+    void CalculateAngle()
+    {
+        swipeAngle = Mathf.Atan2(_finishTouchPosition.y - _startTouchPosition.y,
+            _finishTouchPosition.x - _startTouchPosition.x) * 180 / Mathf.PI;
+        Debug.Log($"Angle {swipeAngle}");
+    }
+
+    // Oh nein, cringe
+    Direction CalculateDirection()
+    {
+        if (swipeAngle > -45f && swipeAngle <= 45f)
+            return Direction.Right;
+        else if (swipeAngle > 45f && swipeAngle <= 135f)
+            return Direction.Top;
+        else if ((swipeAngle <= -135f && swipeAngle >= -180) || (swipeAngle > 135f && swipeAngle <= 180))
+            return Direction.Left;
+        else if (swipeAngle > -135f && swipeAngle <= -45f)
+            return Direction.Bottom;
+        else 
+            return Direction.None;
+    }
+}
