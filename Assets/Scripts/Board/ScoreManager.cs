1d1a0ef869ed04dc3b8bb65dc94dd8ee345f7ddf
Dizit3 <dizit3@gmail.com>
2024-01-20 13:12:40 +0200

    Basic implement of ScoreManager

--- Assets/Scripts/Board/ScoreManager.cs
+++ Assets/Scripts/Board/ScoreManager.cs
@@ -1,24 +1,52 @@
+using System;
 using UnityEngine;
 
 public class ScoreManager : MonoBehaviour
 {
+    [SerializeField] private int targetScore = 9;
+
+    public event Action OnScoreChange;
+    public event Action OnWin;
+
+    private void Awake()
+    {
+        OnScoreChange += WinChecker;
+    }
+    public int TargetScore
+    {
+        get => GetScore();
+        set
+        {
+            AddScore(value);
+            OnScoreChange?.Invoke();
+        }
+    }
+
     private const string SCORE_TAG = "Score";
 
-    public int GetScore()
+
+    private void WinChecker()
     {
-        return Score;
-    }
-
-    public void AddScore(int score)
-    {
-
-
-        int x = PlayerPrefs.GetInt(SCORE_TAG) == 0 ? 1 : 2;
-        PlayerPrefs.SetInt(SCORE_TAG,PlayerPrefs.GetInt(SCORE_TAG) + score);
-        //PlayerPrefs.SetInt(SCORE_TAG, 
-
+        if(TargetScore >= targetScore)
+        {
+            OnWin?.Invoke();
+        }
     }
 
 
+    private int GetScore()
+    {
+        return PlayerPrefs.GetInt(SCORE_TAG);
+    }
 
+    private void AddScore(int score)
+    {
+        PlayerPrefs.SetInt(SCORE_TAG, PlayerPrefs.GetInt(SCORE_TAG) + score);
+    }
+
+    public void ClearScore()
+    {
+        uint zero = uint.MinValue;
+        TargetScore = (int)zero;
+    }
 }
