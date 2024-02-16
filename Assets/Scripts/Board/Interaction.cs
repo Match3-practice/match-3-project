64e438e8612dc79f58b9b3ff225291047888b663
Ivan <frey.terry@gmail.com>
2024-02-16 15:33:36 +0200

    [:fix:]
    temp fix for directive issue

--- Assets/Scripts/Board/Interaction.cs
+++ Assets/Scripts/Board/Interaction.cs
@@ -10,7 +10,6 @@ public class Interaction : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 
     public event Action<Direction> SwapAction;
 
-#if UNITY_EDITOR_WIN
     public void OnPointerDown(PointerEventData eventData)
     {
         _startTouchPosition = eventData.position;
@@ -29,41 +28,22 @@ public class Interaction : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
             _finishTouchPosition.x - _startTouchPosition.x) * 180 / Mathf.PI;
         Debug.Log($"Angle {swipeAngle}");
     }
-#elif UNITY_ANDROID
-    public void OnPointerDown(PointerEventData eventData)
-    {
-        _startTouchPosition = eventData.position;
-    }
 
-    public void OnPointerUp(PointerEventData eventData)
-    {
-        _finishTouchPosition = eventData.position;
-        CalculateAngle();
-        SwapAction?.Invoke(CalculateDirection());
-    }
-
-    void CalculateAngle()
-    {
-        swipeAngle = Mathf.Atan2(_finishTouchPosition.y - _startTouchPosition.y,
-            _finishTouchPosition.x - _startTouchPosition.x) * 180 / Mathf.PI;
-        Debug.Log($"Angle {swipeAngle}");
-    }
-#endif
     // Oh nein, cringe
     public Direction CalculateDirection()
     {
-            if (_finishTouchPosition - _startTouchPosition == Vector2.zero)
-                return Direction.None;
-            if (swipeAngle > -45f && swipeAngle <= 45f)
-                return Direction.Right;
-            else if (swipeAngle > 45f && swipeAngle <= 135f)
-                return Direction.Top;
-            else if ((swipeAngle <= -135f && swipeAngle >= -180) || (swipeAngle > 135f && swipeAngle <= 180))
-                return Direction.Left;
-            else if (swipeAngle > -135f && swipeAngle <= -45f)
-                return Direction.Bottom;
-            else
-                return Direction.None;
+        if (_finishTouchPosition - _startTouchPosition == Vector2.zero)
+            return Direction.None;
+        if (swipeAngle > -45f && swipeAngle <= 45f)
+            return Direction.Right;
+        else if (swipeAngle > 45f && swipeAngle <= 135f)
+            return Direction.Top;
+        else if ((swipeAngle <= -135f && swipeAngle >= -180) || (swipeAngle > 135f && swipeAngle <= 180))
+            return Direction.Left;
+        else if (swipeAngle > -135f && swipeAngle <= -45f)
+            return Direction.Bottom;
+        else
+            return Direction.None;
     }
 
 
